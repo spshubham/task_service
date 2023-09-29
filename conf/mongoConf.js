deb6f005dd5c40351d6d4d1c0316e880072e4718
Shubham Patil <30755507+spshubham@users.noreply.github.com>
2023-09-29 09:57:35 +0530

    Update mongoConf.js

--- conf/mongoConf.js
+++ conf/mongoConf.js
@@ -1,4 +1,4 @@
 
 exports.conf = {
-  dbURL: "mongodb+srv://sp:Shubham1998@cluster0.aylzdlp.mongodb.net/?retryWrites=true&w=majority"
+  dbURL: "mongodb+srv://uname:pass@cluster0.aylzdlp.mongodb.net/?retryWrites=true&w=majority"
 };
